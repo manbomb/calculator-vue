61a4f443b36d85b3dd28823179f61335b2328217
Sérgio Lucas Avilla da Silva <sergio@aiqfome.com>
2020-08-04 06:32:38 -0300

    Finish Calculator

--- src/main/Calculator.vue
+++ src/main/Calculator.vue
@@ -1,7 +1,23 @@
 <template>
     <div class="calculator">
-        <Display />
-        <Button />
+        <Display :value="displayValue"/>
+        <Button label="AC" triple @onClick="clearMemory" />
+        <Button label="/" operation @onClick="setOperation" />
+        <Button label="7" @onClick="addDigit" />
+        <Button label="8" @onClick="addDigit" />
+        <Button label="9" @onClick="addDigit" />
+        <Button label="*" operation @onClick="setOperation" />
+        <Button label="4" @onClick="addDigit" />
+        <Button label="5" @onClick="addDigit" />
+        <Button label="6" @onClick="addDigit" />
+        <Button label="-" operation @onClick="setOperation" />
+        <Button label="1" @onClick="addDigit" />
+        <Button label="2" @onClick="addDigit" />
+        <Button label="3" @onClick="addDigit" />
+        <Button label="+" operation @onClick="setOperation" />
+        <Button label="0" double @onClick="addDigit" />
+        <Button label="." @onClick="addDigit" />
+        <Button label="=" operation @onClick="setOperation" />
     </div>
 </template>
 
@@ -10,7 +26,64 @@ import Display from '../components/Display'
 import Button from '../components/Button'
 
 export default {
-    components: { Button, Display }
+    data: function() {
+        return {
+            displayValue: "0",
+            clearDisplay: false,
+            operation: null,
+            values: [0,0],
+            current: 0
+        }
+    },
+    components: { Button, Display },
+    methods: {
+        clearMemory() {
+            Object.assign(this.$data, this.$options.data())
+        },
+        setOperation(operation) {
+            if (this.current === 0) {
+                this.operation = operation
+                this.current = 1
+                this.clearDisplay = true
+            } else {
+                const equals = operation === '='
+                const currentOperation = this.operation
+
+                try {
+                    this.values[0] = eval(
+                        `${this.values[0]} ${currentOperation} ${this.values[1]}`
+                    )
+                } catch (e) {
+                    this.$emit('onError', e)
+                }
+
+                this.values[1] = 0
+                this.displayValue = this.values[0]
+                this.operation = equals ? null : operation
+                this.current = equals ? 0 : 1
+                this.clearDisplay = !equals
+            }
+        },
+        addDigit(n) {
+            if (n === '.' && this.displayValue.includes('.')) {
+                return
+            }
+
+            const clearDisplay = this.displayValue === '0' || this.clearDisplay
+            const currentValue = clearDisplay ? "" : this.displayValue
+            const displayValue = currentValue + n
+
+            this.displayValue = displayValue
+            this.clearDisplay = false
+
+            if (n !== ".") {
+                const i = this.current
+                const newValue = parseFloat(displayValue)
+                this.values[i] = newValue
+            }
+
+        }
+    }
 }
 </script>
 
